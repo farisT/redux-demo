b82a489fd3c3b33147f3de5d626e44568e6bbef8
Faris Tangastani <faris@Fariss-MacBook-Pro.local>
2018-06-07 12:41:13 +0200

    updated readme

--- src/ReduxDemo.js
+++ src/ReduxDemo.js
@@ -6,7 +6,10 @@ class ReduxDemo  extends Component {
     render() {
     
 
-        //Step 2: Reducer : stand and action
+        //!!Step 1: Reducer : stand and action
+        //state takes two things what state is and what action to perform when the state is changed
+        //reducer needs to go in seperate folder!!!
+
         const reducer = (state,action) => {
             if(action.type === "ATTACK") {
                 return action.payload //payload can be anything
@@ -20,11 +23,8 @@ class ReduxDemo  extends Component {
         //Step 1 Store : reducer and state
 
 
-        const store = createStore(reducer,"peace") // "peace" should/could actually be an object
-        
-
-
-
+        const store = createStore(reducer,"peace") // "peace" is our current state 
+        //reducer does whatever its told to do
 
         //Step 3: Subscribe
 
@@ -34,7 +34,7 @@ class ReduxDemo  extends Component {
 
         //Step 4: Dispatch action
         //usually an object
-
+        //The dispatch is also in its own folder 
         store.dispatch({type:"ATTACK", payload:"Iron Man"})
         store.dispatch({type:"GREENATTACK", payload:"HULK"})
 
